cc70bb93aaa64956cb092ddf729cc1e203d7f6f7
陳伯如 <boru.chen@tpisoftware.com>
2023-01-20 12:57:27 +0800

    init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,10 @@
-import './globals.css'
+import './globals.css';
+import Head from 'next/head';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	return (
+		<html lang='en'>
+			<body>{children}</body>
+		</html>
+	);
 }
